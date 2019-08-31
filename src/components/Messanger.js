149074515278ffeb5931f628dfc2c2780a430036
Jimmique <jimmique88@gmail.com>
2019-08-31 03:01:36 -0700

    styled messenger

--- src/components/Messanger.js
+++ src/components/Messanger.js
@@ -3,6 +3,7 @@ import Sidebar from './Sidebar';
 import Chat from './Chat';
 import moment from 'moment';
 
+
 import initialState from '../seedData/initialState';
 
 class Messanger extends React.Component {
